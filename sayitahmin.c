de952275e0205e896fa0a1f2771aaef7762ee314
Slankss <45669899+Slankss@users.noreply.github.com>
2018-12-12 11:42:03 +0300

    Add files via upload

---
+++ sayitahmin.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <conio.h>
+
+
+int main(int argc, char *argv[]) {
+	
+
+int random[30],sayi,i,a;
+	
+	printf("Bir sayi gir: "); scanf("%d",&sayi);
+	for(i=0;i<30;i++)
+	{
+    random[i]=rand()%100 ;
+	
+	if(sayi==random[i])
+	{
+	a=1;
+	break;
+	
+	
+	
+	}
+	else{
+	a=0;
+	}
+	
+	}
+	
+	if(a==1)
+	{
+	printf("Tebrikler Canim");
+	}
+	
+	else{
+	printf("Uzgunum Canim");
+	}	
+	
+	
+	return 0;
+}
